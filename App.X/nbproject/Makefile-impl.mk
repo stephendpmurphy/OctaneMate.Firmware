6a750f8437452b4b6ea910d6931084b0642dc99a
murphtech <smurphy@murphy.technology>
2017-10-19 20:04:11 -0600

    Added MPLABX project for main App & Updated gitignore

--- App.X/nbproject/Makefile-impl.mk
+++ App.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Moltres_App.X
+PROJECTNAME=App.X
 
 # Active Configuration
 DEFAULTCONF=default
--- App.X/nbproject/Package-default.bash
+++ App.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Moltres_App.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Moltres_App.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=moltresapp.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/App.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=App.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=app.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/moltresapp.x/bin
+makeDirectory ${TMPDIR}/app.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/moltresapp.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/app.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/moltresapp.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/app.x.tar *
 checkReturnCode
 
 # Cleanup
