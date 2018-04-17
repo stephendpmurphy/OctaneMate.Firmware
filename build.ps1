88f342c2bb28d2cc356251f887e94ce2b54495e4
smurphy <stephendpmurphy@msn.com>
2018-04-16 20:45:59 -0600

    Explicitly give the filepath for AtmelStudio.exe

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-AtmelStudio.exe App.atsln /rebuild debug /out BuildOutput_Debug.txt | Out-Null
+C:\Program Files (x86)\Atmel\Studio\7.0\AtmelStudio.exe App.atsln /rebuild debug /out BuildOutput_Debug.txt | Out-Null
 type BuildOutput_Debug.txt
