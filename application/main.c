ae85cda54aead6fd57e0e9ddd0d29587c3dbf03c
smurphy <stephendpmurphy@msn.com>
2018-06-17 15:50:01 -0600

    Starting over

---
+++ application/main.c
@@ -0,0 +1,21 @@
+/*
+ * application.c
+ *
+ * Created: 6/17/2018 3:28:37 PM
+ * Author : Steve
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
